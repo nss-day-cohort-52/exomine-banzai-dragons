0199cf32d8a7d88c5a57896e9f5372c1c04f34fc
lacizeidler <laci.zeidler@gmail.com>
2021-10-26 14:17:02 -0500

    Put html into the exomine.js

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -3,6 +3,31 @@ export const Exomine = () => {
     return `
         <h1>Exomine</h1>
 
-        <se
+        <section id = "top-section">
+            <div class = "section governor">
+            <article>
+                <h2>Choose Governor</h2>
+                
+            </article>
+            <article class = "section facility">
+                <h2>Choose Facility</h2>
+                
+            </article>
+            </div>
+            <div class = "section colony-resources">
+                <h2>Available Resources for Colony</h2>
+            
+            </div>
+        </section>
+        <section id="bottom-section">
+            <div class = "section minerals">
+                <h2>Minerals at Facility</h2>
+
+            </div>
+            <div class ="section cart">
+                <h2>Cart</h2>
+
+            </div>
+        </section>
     `
 }
