803926692e5d256c5f010803fee3deac918cdcf6
lacizeidler <laci.zeidler@gmail.com>
2021-10-27 15:12:38 -0500

    just some comments

---
+++ scripts/cart.js
@@ -0,0 +1,2 @@
+// This modules single responsibiliy is to display the selected input values from Minerals at Facility and transfer them to the transient state object in the database.js and display them in HTML representation in the Cart. 
+
